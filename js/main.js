5ed30b413dcec11aefa5b661c7e92d2db8ed5b07
PDogan_G15 <2025236602@qq.com>
2026-05-05 22:07:14 +0800

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* ===== Active Nav Link ===== */
+document.addEventListener("DOMContentLoaded", function () {
+  var page = window.location.pathname.split("/").pop() || "index.html";
+  if (page === "" || page === "/") page = "index.html";
+  document.querySelectorAll(".nav-links a[data-page]").forEach(function (a) {
+    a.classList.toggle("active", a.getAttribute("data-page") === page);
+  });
+});
